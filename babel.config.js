8a16c3512e12dcec75e411995e54f8469ef216ae
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2022-07-23 03:13:40 +0700

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    [
+      'babel-plugin-transform-remove-imports',
+      {
+        test: ['react-toastify', 'exceljs', 'file-saver'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
